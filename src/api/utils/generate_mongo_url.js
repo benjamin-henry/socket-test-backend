02e5d5b34e88be4ca81e5457b53e00aff13728bc
unknown <benjamin.henry230292@gmail.com>
2020-09-26 17:53:12 +0200

    first commit

--- src/api/utils/generate_mongo_url.js
+++ src/api/utils/generate_mongo_url.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 const generate_mongo_url =  (dbname) => {
-    return `mongodb+srv://${process.env.ADMIN_USERNAME}:${process.env.ADMIN_PASSWORD}@cluster0.sjkw7.gcp.mongodb.net/${dbname}?retryWrites=true&w=majority`
+    return `mongodb+srv://4l4rmpu5h3radmin:5sqvt2zMuhBmtOEc@cluster0.sjkw7.gcp.mongodb.net/${dbname}?retryWrites=true&w=majority`
 };
 
 module.exports = generate_mongo_url
